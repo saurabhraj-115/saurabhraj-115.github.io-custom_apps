95188f5107529caa2697d376627610c864c4833d
Saurabh Raj <saurabhraj115@gmail.com>
2022-12-14 00:04:53 +0530

    logged_in page

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
+var email;
 function processScript(form){
-    var email = form.email.value;
-    alert(email)
-    window.open(https://saurabhraj-115.github.io/saurabhraj-115.github.io-custom_apps/logged_in.html);
+    email = form.email.value;
+    window.open("https://saurabhraj-115.github.io/saurabhraj-115.github.io-custom_apps/logged_in.html");
 }
