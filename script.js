26d5b62e68d127beba8a045e8c628c50a8a03d31
Saurabh Raj <saurabhraj115@gmail.com>
2022-12-14 14:22:35 +0530

    LC embed with hash

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 var email;
+var user_id;
 function processScript(form){
-    email = form.email.value;
+    // user_id = form.user_id.value;
+    // email = form.email.value;
     window.open("https://saurabhraj-115.github.io/saurabhraj-115.github.io-custom_apps/logged_in.html");
 }
+
